83aa354d4d6838daeef8ab9f632ed045c6fbab71
ka-pe <io@kape.ga>
2017-02-28 19:28:06 +0800

    Initial commit. [Submodule Testing]

---
+++ ModelBase.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace PP.Data.Models
+{
+    public abstract class ModelBase<T> : INotifyPropertyChanged, IDataErrorInfo
+    {
+        public bool IsDeleted { get; set; }
+        
+        private long _id;
+
+        /// <summary>
+        /// RowId
+        /// </summary>
+        [PrimaryKey]
+        public long Id
+        {
+            get { return _id; }
+            set
+            {
+                if(value == _id)
+                    return;
+                _id = value;
+            }
+        }
+        
+        #region IDataErrorInfo
+        
+
+        [Ignore]
+        public bool IsValid => GetIsValid();
+
+        protected virtual bool GetIsValid()
+        {
+            return true;
+        }
+
+        protected virtual string GetErrorInfo(string prop)
+        {
+            return null;
+        }
+
+        string IDataErrorInfo.this[string columnName] => GetErrorInfo(columnName);
+
+        string IDataErrorInfo.Error => null;
+
+        #endregion
+
+#region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public bool NotifyPropertyChanged { get; set; } = true;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (!NotifyPropertyChanged) return;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+#endregion
+
+    }
+
+    public class NonClonable : Attribute
+    {
+        
+    }
+}
---
+++ ModelExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System.Data.SQLite;
+using FastMember;
+
+namespace PP.Data.Models
+{
+    public static class ModelExtensions
+    {
+
+
+        public static void Restore<T>(this T item, SQLiteConnection con) where T:ModelBase<T>
+        {
+            var model = con.GetById<T>(item.Id);
+            item.Sync(model);
+        }
+
+        public static void Sync<T>(this T target, T source)
+        {
+            var type = TypeAccessor.Create(typeof(T));
+            var members = type.GetMembers();
+
+            var oTarget = ObjectAccessor.Create(target);
+            var oSource = ObjectAccessor.Create(source);
+
+            foreach(var member in members)
+            {
+                if(member.Name == "Item") continue;
+                if(member.IsDefined(typeof(NonClonable))) continue;
+                if (!(typeof(T).GetProperty(member.Name)?.CanWrite ?? false)) continue;
+                
+                oTarget[member.Name] = oSource[member.Name];
+            }
+        }
+
+        public static T Clone<T>(this T obj)
+        {
+            var type = TypeAccessor.Create(typeof(T));
+            var clone = type.CreateNew();
+            var newObj = ObjectAccessor.Create(clone);
+            var oldObj = ObjectAccessor.Create(obj);
+
+            var members = type.GetMembers();
+            foreach(var member in members)
+            {
+                if(member.Name == "Item") continue;
+                if(member.IsDefined(typeof(NonClonable))) continue;
+                if(typeof(T).GetProperty(member.Name)?.CanWrite ?? false)
+                    newObj[member.Name] = oldObj[member.Name];
+            }
+
+            return (T)clone;
+        }
+    }
+}
